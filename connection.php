5d0b22ae74948728fac3ac475dc75e14b249d148
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2020-12-20 11:56:21 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,17 +1,13 @@
 <?php
 	$Servername = "localhost";
-//SERVER DATABASE DETAILS TO BE FILLED IN
-
-//	$username = "";
-//	$password = "";
-//	$con = mysqli_connect($Servername,$username,$password); //server connect
-//	$dbcon = mysqli_select_db($con,"db_data_user");
-//	if(!$con){
-//	echo "Connection not established to Database";
-//	die();
-//	}
-//	else
+	$username = "mw6mzy0jq0q4";
+	$password = ""; //Password Not GIven for Security Purpose
+ 	$con = mysqli_connect($Servername,$username,$password); //server connect
+	$dbcon = mysqli_select_db($con,"db_data_user");
+	if(!$con){
+	echo "Connection not established to Database";
+	die();
+	}
+	else
 	//	echo "Successful";
-
-
 ?>
