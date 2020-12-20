63b9da999a93c486daf6b226869e8d379ef148fa
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2020-12-20 11:57:06 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,21 +1,21 @@
 <?php
-//	include "connection.php";
+  include "connection.php";
   $Name = $_REQUEST['Name'];
-	$Email = $_REQUEST['Email'];
+  $Email = $_REQUEST['Email'];
   $Phone = $_REQUEST['Phone'];
   $Message = $_REQUEST['Message'];
 
   //TO BE DONE ONCE CONNECTED TO DATABASE
 
-	//$sql = "INSERT INTO ____ ".
-  //    "(Name,Email,Phone,Message) ".
-  //    "VALUES ('$Name','$Email','$Phone','$Messaage')";
+  $sql = "INSERT INTO ecell ".
+      "(Name,Email,Phone,Message) ".
+    "VALUES ('$Name','$Email','$Phone','$Messaage')";
 
-   //$retval = mysqli_query( $con,$sql );
+   $retval = mysqli_query( $con,$sql );
 
-   //if(! $retval ) {
-  //    die('Could not enter data: ' . mysqli_error($con));
-  // }
+   if(! $retval ) {
+      die('Could not enter data: ' . mysqli_error($con));
+   }
 
    echo "Entered '$Name' data successfully\n";
 
